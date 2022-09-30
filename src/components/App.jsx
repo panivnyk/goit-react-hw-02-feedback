d1386e903159e4e1c7beff579668cf660ee3a7b5
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-09-30 13:05:47 +0300

    add HTML and styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+// import styled
+import { Div, Button, P, Span } from 'components/App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Div>
+      <P>Please leave feedback</P>
+      <div>
+        <Button>Good</Button>
+        <Button>Neutral</Button>
+        <Button>Bad</Button>
+      </div>
+      <P>Statistics:</P>
+      <Span>Good:</Span>
+      <Span>Neutral:</Span>
+      <Span>Bad:</Span>
+    </Div>
   );
 };
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+  margin-left: 5px;
+  background-color: #eee;
+  font-size: 20px;
+  cursor: pointer;
+  border: none;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
+    0px 2px 1px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+`;
+
+export const P = styled.p`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const Span = styled.span`
+  margin-bottom: 10px;
+  font-size: 20px;
+`;
